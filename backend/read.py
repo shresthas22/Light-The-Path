ff758d58c1a5b2510c4da8cb2cf4edd072b547e5
shresthas22 <shresthas22@mymail.vcu.edu>
2020-06-10 13:52:48 -0400

    .gitignore fix

--- backend/read.py
+++
@@ -1,9 +0,0 @@
-from connection import app
-
-from flask import jsonify
-from flask import request
-
-@app.route('/readValue', methods=['GET'])
-def read_value():
-  return jsonify({'result' : 'FOUND'})
-  
